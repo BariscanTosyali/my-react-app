91ddd932f8c8ba2e1404c4c3009d8a796e665e7d
BariscanTosyali <bariscantosyali@gmail.com>
2024-07-05 00:21:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I can't Learn React
         </a>
       </header>
     </div>
